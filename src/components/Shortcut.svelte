7ddae8c7df9f8b0f0b5676aadedcbd3f8f4d1ad0
Afterlifepro <96529802+Afterlifepro@users.noreply.github.com>
2024-09-06 22:49:57 +0100

    minor tweaks
    - tbh i cant really explain this just kinda. schmoove it or smthn idk lol
    ik this is a shit commit sorrg :(

---
+++ src/components/Shortcut.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { apps } from "../store";
+
+  export let icon = "";
+  export let href;
+  export let name;
+  export let external = false;
+</script>
+
+<button
+  class="shortcut"
+  on:click={!external
+    ? () => {
+        apps.newApp = { template: href, source: "shortcut" };
+      }
+    : () => {
+        window.open(href);
+      }}
+>
+  <img src={icon} alt="" />
+  <h1>
+    {name}
+    {#if external}
+      <img src={icon} alt="external" />
+    {/if}
+  </h1>
+</button>
+
--- src/components/renderer.jsx
+++ src/components/renderer.jsx
@@ -5,29 +5,15 @@ import Window from "./Window";
 export default function Render() {
   const $openApps = useStore(openApps);
 
-  if (Object.keys($openApps.apps).length === 0) {
-    return (
-      <div>
-        <ul>
-          <li>No Apps Open</li>
-        </ul>
-      </div>
+  let result = [];
+
+  for (const [i, app] of Object.entries($openApps.apps)) {
+    result.push(
+      <Window title={app.title} layer={app.layer} id={i} key={i}>
+        {app.content}
+      </Window>
     );
   }
 
-  let result = []
-
-  for (const [i, app] of Object.entries($openApps.apps)) {
-    result.push(<Window title={app.title} layer={app.layer} id={i} key={i}>
-      {app.content}
-    </Window>)
-  }
-
-  return (
-    <div>
-      <ul>
-        {result}
-      </ul>
-    </div>
-  );
+  return <>{result}</>;
 }
